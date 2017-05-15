d8d5ebb2d6fd1bd925a60ee4b3a0dd428cb10d0a
stan-ley <protectionismz@gmail.com>
2017-05-14 22:49:00 -0400

    Update Class.py
    Changed the times within the class element to the Time type

--- Class.py
+++ Class.py
@@ -1,15 +1,16 @@
+from Time import *
+
 class Class:
     def __init__(self, ctype, day, start, end, room, prof, notes):
         self.ctype = ctype
         self.day = 'N/A' if day is None else day
-        self.start = 'N/A' if start is None else start
-        self.end = 'N/A' if end is None else end
+        self.time = Time(start, end)
         self.room = 'N/A' if room is None else room
         self.prof = 'N/A' if prof is None else prof
         self.notes = 'N/A' if notes is None else notes
 
     def __str__(self):
         return 'Session: ' + self.ctype + " | Day: " + self.day + \
-               " | Start: " + self.start + " | End: " + self.end + \
+               " | Start: " + self.time.get_start() + " | End: " + self.time.get_end() + \
                " | Room: " + self.room + " | Prof: " + self.prof + \
                " | Notes: " + self.notes
